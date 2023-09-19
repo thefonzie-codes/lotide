c4ab9a0b3635f54719b49d0c94cfc88cd6eb21bc
Alfonso Banzon <alfonsobanzon@gmail.com>
2023-09-19 22:25:53 +0000

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+// code for epArrays
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// assertArraysEqual function
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
