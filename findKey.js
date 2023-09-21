801d905be9a3a49f05bf02fc19fdfe26e1c21f22
Alfonso Banzon <alfonsobanzon@gmail.com>
2023-09-21 23:32:18 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,43 @@
+// assertEqual
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//
+
+const findKey = function(object, callback) {
+  let keys = Object.keys(object);
+  for (let key of keys) {
+    if (callback(object[key])) {
+      return key;
+    } 
+  }
+}
+
+/// Test 1
+
+let phone1 = {
+  brand: "nokia",
+  model: "3310",
+  colour: "black"
+}
+
+let blackPhone = findKey(phone1, color => color === "black");
+
+console.log(assertEqual((blackPhone), 'colour'));
+
+/// Test 2
+
+let cpu = {
+  cpu: "13700k",
+  clock: 3.7,
+  boost: 5,
+}
+
+let over5k = findKey(cpu, boost => boost > 4);
+console.log(assertEqual((over5k), 'boost'));
